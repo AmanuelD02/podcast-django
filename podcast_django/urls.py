4196f304844aab196fa04985e0035173cc0d9380
AmanuelD02 <amanueldebebe1000@gmail.com>
2021-12-14 23:40:35 +0300

    create and run user, channel and subscription migration

--- podcast_django/urls.py
+++ podcast_django/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/api/v1/user/','users.urls'),
+    path('/api/v1/user/',include('users.urls')),
+    path('/api/v1/channel/',include('channels.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
