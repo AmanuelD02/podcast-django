2ae451cc232ae827e597dbee3a984ca3540b44d4
AmanuelD02 <amanueldebebe1000@gmail.com>
2022-01-11 14:36:06 +0300

    FIX bug in audio Serializer

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -4,5 +4,5 @@ from .models import Rating
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
-        fields = ['id','audio_id','user_id','rating']
+        fields = ['id','channel_id','user_id','rating']
         read_only_fields = ['id']
