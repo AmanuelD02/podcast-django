d731e41c38242b0e71ad4aa082ecc4075dbd0ab5
cd <amanueldebebe1000@gmail.com>
2022-01-12 16:56:03 +0300

    allowed host

--- podcast_django/settings.py
+++ podcast_django/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-or%b1!3g2*_e1+5lw&=eej3lux5*52al$1gl-)l)8t@+c(l5b5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '192.168.0.197','192.168.0.155' ,'smile-db.herokuapp.com']
 
 
 # Application definition
